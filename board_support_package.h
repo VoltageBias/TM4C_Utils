493676d1881a1a220bc16d8042d2135f22d1037f
VoltageBias <gilbertwaltoon@gmail.com>
2016-10-23 11:58:52 +0100

    First commit
    BSP for the TI TM4C123GXL

---
+++ board_support_package.h
@@ -0,0 +1,27 @@
+//////////////////////////////////////////////////////////////////////////////////
+//
+// Voltage Bias - the Blog.
+// Author: Gilbert Waltoon
+//
+// Released under a GNU General Public License v3.0
+//
+// FILENAME : board_support_package.h
+// PURPOSE  : BSP for the TI TM4C123GXL
+//
+// Version 0: Oct 22, 2016
+//
+////////////////////////////////////////////////////////////////////////////////
+
+
+#ifndef __board_support_package_H__
+#define __board_support_package_H__
+
+/* LEDS */
+#define LED_RED   (1U << 1)
+#define LED_BLUE  (1U << 2)
+#define LED_GREEN (1U << 3)
+
+/* system clock [Hz] */
+#define SYS_CLOCK_HZ 16000000U
+
+#endif // __board_support_package_H__
