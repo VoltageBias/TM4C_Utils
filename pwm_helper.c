e2ad9c03949867dfebabad3dc216d037fa00b24a
DaddyDog <DaddyDog@TheDogHouse.com>
2017-05-01 17:14:34 +0100

    Added example and re-defined fPwmStart() to accept target frequency and duty cycle arguments

--- pwm_helper.c
+++ pwm_helper.c
@@ -5,10 +5,11 @@
 //
 // Released under a GNU General Public License v3.0
 //
-// FILENAME : adc_helper.c
-// PURPOSE  : A set of helper functions to operate the TM4C123 ADC
+// FILENAME : pwm_helper.c
+// PURPOSE  : A set of helper functions to operate the TM4C123 PWM
+//          : currently only outputs on pins PB6, PB7
 //
-// Version 0: Oct 30, 2016
+// Version 0: 1st May 2017
 //
 ////////////////////////////////////////////////////////////////////////////////
 #include "TM4C123GH6PM.h"
@@ -17,45 +18,53 @@
 #include "uart_helper.h"
 #include "pwm_helper.h"
 
+////////////////////////////////////////////////////////////////////////////////
+//////////////                fPWMExample() follows            ////////////
+////////////////////////////////////////////////////////////////////////////////
+
+// Call to run an example. Writes ADC values to the UART@115200Baud
+void fPWMExample(){
+    fStartUART(UART0, UART0_CFG, baud115200);
+    fPrintStringUART(UART0, "\n\r Starting PWM Example.....: ");
+
+// For a Desired PWM Frequency and Duty [0..1] pass in:
+//
+// ui32_PWM0LOAD = ( SYS_CLOCK_HZ / 2xDesired-frequency ) - 1
+// ui32_PWM0CMPA = ((1 - Desired-duty-PWM_A) x (SYS_CLOCK_HZ / 2xDesired-freq.))-1
+// ui32_PWM0CMPA = ((1 - Desired-duty-PWM_B) x (SYS_CLOCK_HZ / 2xDesired-freq.))-1
+//
+// For example, for desired 25kHz, PWMa= 0.25, PWMb = 0.75 duty, SYS_CLOCK_HZ=16MHz
+//   ui32_PWM0LOAD = 16,000,000/(2x25,000) - 1 =  319
+//   ui32_PWM0CMPA = ( (1-0.25) x 320 ) - 1 = 239
+//   ui32_PWM0CMPB = ( (1-0.75) x 320 ) - 1 = 79
+
+    fStartPWM(319, 239, 79);
+    while(1){};
+    return;
+ }
+////////////////////////////////////////////////////////////////////////////////
+//////////////                fPWMExample() ends                    ////////////
+////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////////////////////////
-//////////////                fConstructPWMcfg() follows            ////////////
+//////////////                fStartPWM() follows                   ////////////
 ////////////////////////////////////////////////////////////////////////////////
-pPWMcfg_t fConstructPWMcfg(                                         \
-		uint32_t uint32RCGCGPIO_Rx,                            \
-		GPIOA_Type* gpioGPIOx,                                 \
-		uint32_t uint32GPIODIR_DIR_x,                          \
-		uint32_t uint32GPIOAFSEL_AFSEL_x,                      \
-		uint32_t uint32GPIODEN_DEN_x,                          \
-		uint32_t uint32GPIOAMSEL_GPIOAMSEL_x   )
-{
-	static PWMcfg_t strucPWMcfgx; //declare a (static) struc variable...
-									// ...fill it...
-	strucPWMcfgx.RCGCGPIO_Rx           = uint32RCGCGPIO_Rx;
-	strucPWMcfgx.GPIOx                 = gpioGPIOx;
-	strucPWMcfgx.GPIODIR_DIR_x         = uint32GPIODIR_DIR_x;
-	strucPWMcfgx.GPIOAFSEL_AFSEL_x     = uint32GPIOAFSEL_AFSEL_x;
-	strucPWMcfgx.GPIODEN_DEN_x         = uint32GPIODEN_DEN_x;
-	strucPWMcfgx.GPIOAMSEL_GPIOAMSEL_x = uint32GPIOAMSEL_GPIOAMSEL_x;
+void fStartPWM(uint32_t ui32_PWM0LOAD,        \
+               uint32_t ui32_PWM0CMPA,        \
+               uint32_t ui32_PWM0CMPB ) {
+ // Shows shows how to initialize PWM Gen. 0 with a 25-kHz, 25% duty cycle on
+ // MnPWM0 pin, and 75% duty on MnPWM1 pin. Assumes system clock is 16 MHz.
+ // M0PWM0 = PB6(4) pin no.1
+ // M0PWM1 = PB7(4) pin no.4
 
-	return &strucPWMcfgx;              // ...and return a pointer to it
-}
-////////////////////////////////////////////////////////////////////////////////
-//////////////                fConstructPWM() ends                 ////////////
-////////////////////////////////////////////////////////////////////////////////
-//void fStartPWM(PWMcfg_t *PWMcfgx)
-void fStartPWM(){
-// M0PWM0 = PB6(4) pin no.1
-// M0PWM1 = PB7(4) pin no.4
-
-//	p1239, Enable the PWM clock by writing 0x0010.0000 to the RCGC0 p456
-	// SYSCTL->RCGC0 = RCGC0_PWM0;
-	 SYSCTL->RCGCPWM = (1U<<0);
-	 //RCGCPWM
+//  p1239, Enable the PWM clock by writing 0x0010.0000 to the RCGC0 p456
+     SYSCTL->RCGC0 = RCGC0_PWM0;
+    // SYSCTL->RCGCPWM = (1U<<0);
+     //RCGCPWM
 
 // Enable the clock to the appropriate GPIO module via RCGC2 (p.464)
-     //SYSCTL->RCGC2 |= RCGC2_GPIOB;
-	SYSCTL->RCGCGPIO |= (1U<<1);
+     SYSCTL->RCGC2 |= RCGC2_GPIOB;
+    //SYSCTL->RCGCGPIO |= (1U<<1);
 
 // In GPIO module, enable the  pins for alt. func. using GPIOAFSEL (p671)
     GPIOB->AFSEL |= ( GPIOAFSEL_AFSEL_6 | GPIOAFSEL_AFSEL_7 );
@@ -89,69 +98,60 @@ void fStartPWM(){
 // ticks per period.  Use this value to set the PWM0LOAD register.
 // In Count-Down mode, set the LOAD field in PWM0LOAD to the requested period-1.
 //  Write the PWM0LOAD register with a value of [sic.] 0x0000.018F (399)
-     PWM0->_0_LOAD = 0x1F3; //499
+     PWM0->_0_LOAD = ui32_PWM0LOAD;
 
 //  Set the pulse width of the MnPWM0 pin for a 25% duty cycle.
 //  Write the PWM0CMPA register with a value of 0x0000.012B (299)
-     PWM0->_0_CMPA = 0x176; //374
+     PWM0->_0_CMPA =  ui32_PWM0CMPA;
 
 //Set the pulse width of the MnPWM1 pin for a 75% duty cycle.
 // Write the PWM0CMPB register with a value of 0x0000.0063 (99)
-    PWM0->_0_CMPB = 0x7C; //125
+    PWM0->_0_CMPB = ui32_PWM0CMPB;
 
 // Start the timers in PWM generator 0.
 // Write the PWM0CTL register with a value of 0x0000.0001.
-	//PWM0->CTL = PWMCTL_GLOBALSYNC0;
-	PWM0->_0_CTL = PWMCTL_GLOBALSYNC0;
+    //PWM0->CTL = PWMCTL_GLOBALSYNC0;
+    PWM0->_0_CTL = PWMCTL_GLOBALSYNC0;
 
 // Enable PWM outputs.
 // Write the PWMENABLE register with a value of 0x0000.0003.
-	PWM0->ENABLE = ( PWMENABLE_PWM0EN | PWMENABLE_PWM1EN );
+    PWM0->ENABLE = ( PWMENABLE_PWM0EN | PWMENABLE_PWM1EN );
 
 
-	return;
+    return;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-void fStart2PWM(){
-
-    SYSCTL->RCC = 0x07803D40;
-
-    SYSCTL->RCGCPWM = 0x01;
-
-    GPIOB->PCTL = 0x04003300;
-
-    GPIOB->DIR = 0x0;
-
-    GPIOB->AFSEL = 0x40;
-
-    GPIOB->DR2R = 0xFF;
-    GPIOB->DR4R = 0x0;
-    GPIOB->DR8R = 0x0;
-
-    GPIOB->ODR = 0x0;
-    GPIOB->PDR = 0x0;
-    GPIOB->PUR = 0x0;
-    GPIOB->DEN = 0x40;
-
-    PWM0->_0_CTL = 0x02;
-    PWM0->_0_GENA = 0xB0;
-    PWM0->_0_GENB = 0xB00;
-
-    PWM0->_0_LOAD = 0x7D00;
-
-    PWM0->_0_CMPA = 0x5DC0;
-
-    PWM0->ENABLE = 0x01;
-
-    PWM0->_0_CTL = 0x03;
-
-    //PWM0->_0_RIS = 0x0F;
+//////////////                fStartPWM() ends                      ////////////
+////////////////////////////////////////////////////////////////////////////////
 
 
 
+/* todo Approach of supplying a constructor at compile time not yet implemnted
+////////////////////////////////////////////////////////////////////////////////
+//////////////                fConstructPWMcfg() follows            ////////////
+////////////////////////////////////////////////////////////////////////////////
+pPWMcfg_t fConstructPWMcfg(                                         \
+       uint32_t uint32RCGCGPIO_Rx,                            \
+       GPIOA_Type* gpioGPIOx,                                 \
+       uint32_t uint32GPIODIR_DIR_x,                          \
+       uint32_t uint32GPIOAFSEL_AFSEL_x,                      \
+       uint32_t uint32GPIODEN_DEN_x,                          \
+       uint32_t uint32GPIOAMSEL_GPIOAMSEL_x   )
+{
+   static PWMcfg_t strucPWMcfgx; //declare a (static) struc variable...
+                                   // ...fill it...
+   strucPWMcfgx.RCGCGPIO_Rx           = uint32RCGCGPIO_Rx;
+   strucPWMcfgx.GPIOx                 = gpioGPIOx;
+   strucPWMcfgx.GPIODIR_DIR_x         = uint32GPIODIR_DIR_x;
+   strucPWMcfgx.GPIOAFSEL_AFSEL_x     = uint32GPIOAFSEL_AFSEL_x;
+   strucPWMcfgx.GPIODEN_DEN_x         = uint32GPIODEN_DEN_x;
+   strucPWMcfgx.GPIOAMSEL_GPIOAMSEL_x = uint32GPIOAMSEL_GPIOAMSEL_x;
 
-
- return;
+   return &strucPWMcfgx;              // ...and return a pointer to it
 }
-
+////////////////////////////////////////////////////////////////////////////////
+//////////////                fConstructPWM() ends                 ////////////
+////////////////////////////////////////////////////////////////////////////////
+//void fStartPWM(PWMcfg_t *PWMcfgx)
+*/
--- pwm_helper.h
+++ pwm_helper.h
@@ -18,10 +18,41 @@
 #include "TM4C123GH6PM.h"
 #include "tm4c_register_fields.h"
 
+
+////////////////////////////////////////////////////////////////////////////////
+//                PWM Helper function prototypes follow                       //
+////////////////////////////////////////////////////////////////////////////////
+
+// Call to run an example. Writes to the UART@115200Baud
+void fPWMExample();
+
+////////////////////////////////////////////////////////////////////////////////
+// fstartPWM()...
+//
+// For a Desired PWM Frequency and Duty [0..1] pass in:
+//
+// ui32_PWM0LOAD = ( SYS_CLOCK_HZ / 2xDesired-frequency ) - 1
+// ui32_PWM0CMPA = ((1 - Desired-duty-PWM_A) x (SYS_CLOCK_HZ / 2xDesired-freq.))-1
+// ui32_PWM0CMPA = ((1 - Desired-duty-PWM_B) x (SYS_CLOCK_HZ / 2xDesired-freq.))-1
+//
+// For example, for desired 25kHz, PWMa= 0.25, PWMb = 0.75 duty, SYS_CLOCK_HZ=16MHz
+//   ui32_PWM0LOAD = 16,000,000/(2x25,000) - 1 =  319
+//   ui32_PWM0CMPA = ( (1-0.25) x 320 ) - 1 = 239
+//   ui32_PWM0CMPB = ( (1-0.75) x 320 ) - 1 = 79
+
+void fStartPWM(uint32_t ui32_PWM0LOAD,        \
+               uint32_t ui32_PWM0CMPA,        \
+               uint32_t ui32_PWM0CMPB );
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+/* todo Approach of supplying a constructor at compile time not yet implemnted
 ////////////////////////////////////////////////////////////////////////////////
 //                Definitions of PWMCfg structures follows                      //
 ////////////////////////////////////////////////////////////////////////////////
 //
+
 // When intialising one of the 2xADC's we need to know which board-pins/ports
 // that ADC uses. We pass this information inside an AINx structure.
 //
@@ -40,34 +71,27 @@
 //        }
 //
 typedef struct {
-					uint32_t RCGCGPIO_Rx; 			 \
-					GPIOA_Type *GPIOx;    			 \
-					uint32_t GPIODIR_DIR_x;          \
-					uint32_t GPIOAFSEL_AFSEL_x;      \
-					uint32_t GPIODEN_DEN_x;   		 \
-					uint32_t GPIOAMSEL_GPIOAMSEL_x;  \
-				} PWMcfg_t, *pPWMcfg_t;
+                    uint32_t RCGCGPIO_Rx;            \
+                    GPIOA_Type *GPIOx;               \
+                    uint32_t GPIODIR_DIR_x;          \
+                    uint32_t GPIOAFSEL_AFSEL_x;      \
+                    uint32_t GPIODEN_DEN_x;          \
+                    uint32_t GPIOAMSEL_GPIOAMSEL_x;  \
+                } PWMcfg_t, *pPWMcfg_t;
 
 
 // Declare the constructor function that assembles a
 // PWM configuration  at compile time
 pPWMcfg_t fConstructPWMcfg(                          \
-			    		uint32_t,                     \
-						GPIOA_Type*,                  \
-						uint32_t,                     \
-						uint32_t,                     \
-						uint32_t,                     \
-						uint32_t                      \
-						 );
-
-
-////////////////////////////////////////////////////////////////////////////////
-//                PWM Helper function prototypes follow                       //
-////////////////////////////////////////////////////////////////////////////////
-
-// Configuare the sample sequence for the ADC defined within the SS_CFG structure
-//void fStartPWM(PWMcfg_t*);
-void fStartPWM();
-void fStart2PWM();
+                        uint32_t,                     \
+                        GPIOA_Type*,                  \
+                        uint32_t,                     \
+                        uint32_t,                     \
+                        uint32_t,                     \
+                        uint32_t                      \
+                         );
+*/
+// // Configuare the sample sequence for the ADC defined within the SS_CFG structure
+// void fStartPWM(PWMcfg_t*);
 
 #endif /* TM4C_UTILS_PWM_HELPER_H_ */
